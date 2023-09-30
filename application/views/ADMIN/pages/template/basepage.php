f314f3579f90c024bd48b4c59c921eaa47638085
ChristelRav <ravmihary@gmail.com>
2023-09-30 18:38:00 +0300

    1

---
+++ application/views/ADMIN/pages/template/basepage.php
@@ -0,0 +1,11 @@
+<?php 
+
+    $this->load->view('ADMIN/pages/template/page-head');
+
+    $this->load->view('ADMIN/pages/template/menu-head');
+
+    $this->load->view('ADMIN/pages/template/menu-bar');
+
+    // $this->load->view('admin/pages/'.$page,$data);
+
+?>
