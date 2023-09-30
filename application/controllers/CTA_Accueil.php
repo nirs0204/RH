e88ac687bd1484c95e3f86d8553efc4a79e6cc37
ChristelRav <ravmihary@gmail.com>
2023-09-30 19:25:55 +0300

    controlleur

--- application/controllers/CTA_Accueil.php
+++ application/controllers/CTA_Accueil.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class CTA_Accueil extends CI_Controller {
 
 	private function viewer($page, $data)
 	{
