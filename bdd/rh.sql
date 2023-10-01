c26d92b40bbfd4c887e7e1da78676076bc9cce90
ChristelRav <ravmihary@gmail.com>
2023-10-01 10:21:38 +0300

    Coef CV + critere RH 1

--- bdd/rh.sql
+++ bdd/rh.sql
@@ -10,8 +10,15 @@ create table client (
 
 create table cv (
     idclient int references client(idclient),
-    diplome decimal(3,2),
-    langue decimal(3,2),
+    diplome1 decimal(3,2),
+    diplome2 decimal(3,2),
+    diplome3 decimal(3,2),
+    diplome4 decimal(3,2),
+    diplome5 decimal(3,2),
+    diplome6 decimal(3,2),
+    langue1 decimal(3,2),
+    langue2 decimal(3,2),
+    langue3 decimal(3,2),
     sexe decimal(3,2),
     Smatri decimal(3,2),
     nom varchar(150),
@@ -48,7 +55,9 @@ create table critere (
     nationalite int,
     sexe int,
     Smatri int,
-    langue int,
+    langue1 int,
+    langue2 int,
+    langue3 int,
     dateFin date,
     debutEnt date
 );
@@ -60,3 +69,30 @@ create table questionnaire (
     reponse int,
     coef int
 );
+
+--------------INSERTION------------------------
+
+-- Critere {
+    --1)diplome
+                  --doctorat (30)      master (25)     licence(20)
+                  --bacc     (15)      bepc   (10)     cepe   (5)
+    --2)langue
+                  --Mlg      (15)      frc    (10)     ang    (5)
+    --3)sexe      
+                  --H        (15)      F      (10)     LGBT   (5)
+    --4)SMatri    
+                  --marie    (15)      Celibat(10)     Divorce(5)
+--}
+
+-- CV {
+    --1)diplome
+                  --doctorat (11)      master (8)     licence(6)
+                  --bacc     (4)      bepc   (2)     cepe   (1)
+    --2)langue
+                  --Mlg      (3)      frc    (2)     ang    (5)
+    --3)sexe      
+                  --H        (2)      F      (2)     LGBT   (1)
+    --4)SMatri    
+                  --marie    (2)      Celibat(2)     Divorce(2)
+--}
+
