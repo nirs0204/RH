66c4fb3e746d7fd817f14773b188cb1320c3ebc1
ChristelRav <ravmihary@gmail.com>
2023-10-02 14:48:23 +0300

    cv insert 1 RH 1

---
+++ application/controllers/CTC_Cv.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class CTC_Cv extends CI_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('MDC_Client');
+        $this->load->helper('main_helper');
+    }
+	private function viewer($page, $data){
+		$v = array(
+			'page' => $page,
+			'data' => $data
+		);
+		$this->load->view('CLIENT/pages/template/basepage', $v);
+	}
+
+    public function index(){
+        $this->viewer('/cv',array());
+    }
+    public function insert(){
+        echo $this->input->post('prenom')."/";
+        echo $this->input->post('tel')."/";
+        echo $this->input->post('add')."/";
+        echo $this->input->post('nom')."/";
+        echo $this->input->post('dtn')."/";
+        echo $this->input->post('exp')."/";
+        echo $this->input->post('diplome')."/";
+        echo $this->input->post('langue1')."/";
+        echo $this->input->post('langue2')."/";
+        echo $this->input->post('langue3')."/";
+        echo $this->input->post('sexe')."/";
+        echo $this->input->post('sm')."/";
+
+        echo 'Succes';
+    }
+}
--- application/views/CLIENT/pages/detail.php
+++ application/views/CLIENT/pages/detail.php
@@ -96,21 +96,7 @@
     </div>
     <!-- page-body-wrapper ends -->
   </div>
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="../../vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/template.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <!-- End custom js for this page-->
+
 </body>
 
 </html>
