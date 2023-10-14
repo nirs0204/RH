e3305484ab5217e431a8145b8a29df85ec586445
ChristelRav <ravmihary@gmail.com>
2023-10-14 12:17:13 +0300

    modif RH 1

--- application/models/MDC_Noteclient.php
+++ application/models/MDC_Noteclient.php
@@ -19,5 +19,19 @@ class MDC_Noteclient extends CI_Model
         $query = $this->db->get();
         return $query->result_array();
     }
+
+    public function note_trier($besoin,$nbr) {
+        $this->db->select("*, (c.diplome + c.langue1 + c.langue2 + c.langue3 + c.sexe + c.Smatri) as total_cv_note, n.idNoteClient, n.noteClient, EXTRACT(YEAR FROM CURRENT_DATE) - EXTRACT(YEAR FROM TO_DATE(c.dtn, 'YYYY-MM-DD')) AS age", false);
+        $this->db->from('cv c');
+        $this->db->join('noteclient n', 'c.idclient = n.idclient');
+        $this->db->join('critere cr', 'cr.idbesoin = c.idbesoin');
+        $this->db->where('c.idbesoin', $besoin);
+        $this->db->order_by('total_cv_note', 'DESC');
+        $this->db->order_by('n.noteclient', 'DESC');
+        $this->db->limit($nbr);
+        $query = $this->db->get();
+        return $query->result();
+    }
+    
 }
 ?>
--- application/views/ADMIN/pages/besoin.php
+++ application/views/ADMIN/pages/besoin.php
@@ -8,7 +8,7 @@
           <div class="container px-6 mx-auto grid">
             <!-- General elements -->
             <h4
-              class="mb-4 text-lg font-semibold text-gray-600 dark:text-gray-300"
+              class="mb-4 text-lg font-semibold text-gray-700 dark:text-gray-200"
             >
               Besoin personnel
             </h4>
