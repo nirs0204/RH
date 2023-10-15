7b8ef51435d6a01cd00d67836ef47e6cbdbcf9ba
ChristelRav <ravmihary@gmail.com>
2023-10-15 10:50:46 +0300

    Page RH 1

--- application/views/ADMIN/pages/news.php
+++ application/views/ADMIN/pages/news.php
@@ -3,22 +3,14 @@
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
 
   <body>
-    <div
-      class="flex h-screen bg-gray-50 dark:bg-gray-900"
-      :class="{ 'overflow-hidden': isSideMenuOpen }">
-      <!-- Desktop sidebar -->
-      
-      <!-- Mobile sidebar -->
-      <!-- Backdrop -->
-    
-      <div class="flex flex-col flex-1 w-full">
+   
         
         <main class="h-full overflow-y-auto">
           <div class="container px-6 mx-auto grid">
             <h2
               class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"
             >
-              Dashboard
+              Annonces
             </h2>
             
             <div class="grid gap-6 mb-8 md:grid-cols-2 xl:grid-cols-5">
