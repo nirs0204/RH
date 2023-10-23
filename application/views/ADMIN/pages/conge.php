cd19cc45771eae29bdb6c6e9a54ebda2470bb4c5
ChristelRav <ravmihary@gmail.com>
2023-10-23 14:54:47 +0300

    conge.  RH 3

--- application/views/ADMIN/pages/conge.php
+++ application/views/ADMIN/pages/conge.php
@@ -1,8 +1,55 @@
 <!DOCTYPE html>
 <html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
- 
+ <?php  $this->load->view('ADMIN/pages/template/page-head'); ?>
   <body>
     <!-- ITY NO CORPS DU SITE -->
+    <div
+      class="flex h-screen bg-gray-50 dark:bg-gray-900"
+      :class="{ 'overflow-hidden': isSideMenuOpen}"
+    >
+      <!-- Desktop sidebar -->
+      <aside
+        class="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block flex-shrink-0"
+      >
+        <div class="py-4 text-gray-500 dark:text-gray-400">
+          <a
+            class="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
+            href="#"
+          >
+            Windmill
+          </a>
+          <ul class="mt-6">
+            <li class="relative px-6 py-3">
+              <a
+                class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                href="index.html"
+              >
+                <svg
+                  class="w-5 h-5"
+                  aria-hidden="true"
+                  fill="none"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  ></path>
+                </svg>
+                <span class="ml-4">Demande de conge</span>
+              </a>
+            </li>
+          </ul>
+          <ul>
+       
+      </aside>
+
+      <!-- Mobile sidebar -->
+    <?php  $this->load->view('ADMIN/pages/template/menu-head'); ?>
+
+    <!-- bar de menu-->
            
     <main class="h-full overflow-y-auto">
           <div class="container px-6 mx-auto grid">
