5f43f5c7219cb4a1f4086a17805d48107835ae09
nirs0204 <118588963+nirs0204@users.noreply.github.com>
2023-10-23 09:51:32 +0300

    demande conge modifie

--- application/controllers/CTA_Conge.php
+++ application/controllers/CTA_Conge.php
@@ -36,16 +36,11 @@ class CTA_Conge extends CI_Controller
         $data['demandeDisponible'] = array();
         $tab = $this->MDA_DemandeConge->getLeaveRequestBy1();
         foreach ($tab as $item) {
-            // Convertir la date de la demande en objet DateTime
             $date_demande = new DateTime($item->datedemande);
             $date_depart = new DateTime($item->datedebut);
-            $date_depart_formatte = $date_depart->format('Y-m-d');
-            // Ajouter 15 jours à la date actuelle
             $limite_date = clone $date_demande;
             $limite_date -> modify('+15 days');
-            // Comparer la date de la demande avec la limite
-            if ($limite_date >= $date_depart_formatte) {
-                // Ajouter la demande au tableau
+            if ($limite_date >= $date_depart) {
                 $data['demandeDisponible'][] = $item;
             }
         }
@@ -54,13 +49,15 @@ class CTA_Conge extends CI_Controller
 
     public function reject_leave_request(){
         $idemploye = $this->input->get('idemploye');
-        $this->MDA_DemandeConge->rejectLeaveRequest($idemploye);
+        $iddemande = $this->input->get('iddemande');
+        $this->MDA_DemandeConge->rejectLeaveRequest($idemploye, $iddemande);
         redirect('CTA_Conge/list_leave_request');
     }
 
     public function approve_leave_request(){
         $idemploye = $this->input->get('idemploye');
-        $this->MDA_DemandeConge->approveLeaveRequest($idemploye);   
+        $iddemande = $this->input->get('iddemande');
+        $this->MDA_DemandeConge->approveLeaveRequest($idemploye, $iddemande);   
         $demande = $this->MDA_DemandeConge->getDemandeConge($idemploye);
         $this->MDA_Conge->updateMin($idemploye,$demande->nbjours);
         redirect('CTA_Conge/list_leave_request');
